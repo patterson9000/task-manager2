8c45f873c54ff339bf62459dd2e1de269b19dc94
patterson9000 <106724462+patterson9000@users.noreply.github.com>
2022-07-29 15:00:27 -0700

    Create task.js

---
+++ task.js
@@ -0,0 +1,15 @@
+class Task{
+    constructor(important,title,dueDate,desc,color,location,status,notification){
+       this.important = important;
+       this.title = title;
+       this.dueDate =dueDate;
+       this.desc = desc;
+       this.color = color;
+       this.location = location;
+       this.status = status;
+       this.notification = notification;
+
+       this.name = "bret";
+
+    }
+}
